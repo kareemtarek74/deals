70b2d13b7ab088ef867618a1ce5f62ada64483a8
kareemtarek74 <157656699+kareemtarek74@users.noreply.github.com>
2024-12-18 12:45:57 +0200

    add function to the scroll to top button

--- lib/Features/Deals/presentation/view/widgets/deals_view_body.dart
+++ lib/Features/Deals/presentation/view/widgets/deals_view_body.dart
@@ -7,16 +7,62 @@ import 'package:deals_page/core/utils/app_images.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class DealsViewBody extends StatelessWidget {
+class DealsViewBody extends StatefulWidget {
   const DealsViewBody({super.key});
+
+  @override
+  State<DealsViewBody> createState() => DealsViewBodyState();
+}
+
+class DealsViewBodyState extends State<DealsViewBody> {
+  final ScrollController scrollController = ScrollController();
+  bool showScrollToTopButton = false;
+
+  @override
+  void initState() {
+    super.initState();
+    scrollController.addListener(scrollListener);
+  }
+
+  @override
+  void dispose() {
+    scrollController.removeListener(scrollListener);
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  void scrollListener() {
+    if (scrollController.offset > 100) {
+      if (!showScrollToTopButton) {
+        setState(() {
+          showScrollToTopButton = true;
+        });
+      }
+    } else {
+      if (showScrollToTopButton) {
+        setState(() {
+          showScrollToTopButton = false;
+        });
+      }
+    }
+  }
+
+  void scrollToTop() {
+    scrollController.animateTo(
+      0.0,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        const CustomScrollView(
+        CustomScrollView(
+          controller: scrollController,
           clipBehavior: Clip.none,
-          slivers: [
+          slivers: const [
             SliverToBoxAdapter(
               child: Column(
                 children: [
@@ -47,15 +93,17 @@ class DealsViewBody extends StatelessWidget {
             ),
           ],
         ),
-        Positioned(
-          bottom: 16,
-          right: 16,
-          child: FloatingActionButton.small(
+        if (showScrollToTopButton)
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: FloatingActionButton.small(
               shape: const CircleBorder(),
-              onPressed: () {},
+              onPressed: scrollToTop,
               backgroundColor: const Color(0xff000080),
-              child: SvgPicture.asset(Assets.imagesArrowUp)),
-        ),
+              child: SvgPicture.asset(Assets.imagesArrowUp),
+            ),
+          ),
       ],
     );
   }
